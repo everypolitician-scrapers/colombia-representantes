9987f1e0b8f3d781481d9e83cfacd3fe24edc31d
Tony Bowden <tony@tmtm.com>
2018-10-08 09:29:06 +0100

    Rewrite for new layout

--- Gemfile
+++ Gemfile
@@ -8,19 +8,18 @@ git_source(:github) { |repo_name| "https://github.com/#{repo_name}.git" }
 
 gem 'nokogiri'
 gem 'open-uri-cached'
-gem 'rake'
 gem 'rest-client'
-gem 'rubocop'
-gem 'scraped', github: 'everypolitician/scraped'
-gem 'scraper_test', github: 'everypolitician/scraper_test'
-gem 'scraperwiki', github: 'openaustralia/scraperwiki-ruby',
-                   branch: 'morph_defaults'
+gem 'scraped', github: 'everypolitician/scraped', branch: 'scraper-class'
+gem 'scraperwiki', github: 'openaustralia/scraperwiki-ruby', branch: 'morph_defaults'
 gem 'table_unspanner', github: 'everypolitician/table_unspanner'
 
 group :test do
   gem 'minitest'
   gem 'minitest-around'
   gem 'minitest-vcr'
+  gem 'rake'
+  gem 'rubocop'
+  gem 'scraper_test', github: 'everypolitician/scraper_test'
   gem 'vcr'
   gem 'webmock'
 end
--- scraper.rb
+++ scraper.rb
@@ -1,48 +1,61 @@
 #!/bin/env ruby
-# encoding: utf-8
+# frozen_string_literal: true
 
 require 'nokogiri'
 require 'scraped'
 require 'scraperwiki'
+require 'irb'
 
 require 'open-uri/cached'
 OpenURI::Cache.cache_path = '.cache'
 
-def noko_for(url)
-  Nokogiri::HTML(open(url).read)
-end
+class MembersPage < Scraped::HTML
+  decorator Scraped::Response::Decorator::CleanUrls
 
-def scrape_list(url)
-  noko = noko_for(url)
-  noko.css('.listado_proyectos a[href*="/representantes/"]/@href').each do |href|
-    link = URI.join url, href
-    scrape_person(link)
+  field :members do
+    member_urls.map do |url|
+      Scraped::Scraper.new(url => MemberPage).scraper.to_h
+    end
+  end
+
+  private
+
+  def member_urls
+    noko.css('.namereplist a/@href').map(&:text)
   end
 end
 
-def scrape_person(url)
-  noko = noko_for(url)
+class MemberPage < Scraped::HTML
+  decorator Scraped::Response::Decorator::CleanUrls
 
-  box = noko.css('#contenido_comision')
-  rhs = noko.css('.contenido_der')
+  field :id do
+    url.split('/').last
+  end
 
-  data = { 
-    id: url.to_s[/idrpr=(\d+)/, 1],
-    name: box.css('.contentheading').text.tidy,
-    party: box.xpath('.//td[contains(.,"Partido o Movimiento")]/following-sibling::td').text.tidy,
-    area: box.xpath('.//td[contains(.,"Circunscripción territorial")]/following-sibling::td').text.tidy,
-    image: rhs.css('.redes_sociales img/@src').first.text,
-    email: rhs.css('a[href*="mailto:"]/@href').text.sub('mailto:',''),
-    tel: rhs.xpath('.//td[contains(.,"Teléfono")]/following-sibling::td').text.tidy,
-    facebook: rhs.css('a[href*="facebook"]/@href').text,
-    twitter: rhs.css('a[href*="twitter"]/@href').text,
-    term: 2014,
-    source: url.to_s,
-  }
-  data[:image] = URI.join(url, URI.encode(URI.decode(data[:image]))).to_s unless data[:image].to_s.empty?
-  puts data.reject { |_, v| v.to_s.empty? }.sort_by { |k, _| k }.to_h if ENV['MORPH_DEBUG']
-  ScraperWiki.save_sqlite([:id, :term], data)
+  field :name do
+    noko.css('title').text.split('|').first.tidy
+  end
+
+  field :image do
+    noko.css('.field--name-field-imagen-representante img/@src').text
+  end
+
+  field :email do
+    noko.css('.field--name-field-correorepresentante a').text.tidy
+  end
+
+  field :area do
+    noko.css('.field--name-field-circunscripcion-territoria').text.tidy
+  end
+
+  field :party do
+    noko.css('.partpolit').text.tidy
+  end
+
+  field :source do
+    url
+  end
 end
 
-ScraperWiki.sqliteexecute('DROP TABLE data') rescue nil
-scrape_list('http://www.camara.gov.co/portal2011/representantes/honorables-representantes?option=com_representantes&view=representantes&limit=0')
+url = 'http://www.camara.gov.co/representantes'
+Scraped::Scraper.new(url => MembersPage).store(:members)
